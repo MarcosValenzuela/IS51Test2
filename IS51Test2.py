97a28edd8bc29576f5157120cf7ae3bec7bcc9b4
MarcosValenzuela <marcosvalezuela@mail.fresnostate.edu>
2020-12-03 16:25:42 -0800

    finished structured-english

---
+++ IS51Test2.py
@@ -0,0 +1,16 @@
+"""
+This program will read a find the number of grades, average grade, and
+
+We first read text file
+from the text file it will count the amount of grades
+then from the text file we sum the grades in total
+we % the sum of grades to the amount of grades to find class average
+we then compare each grade to class average to see who scored above the average
+and sum those that did
+with the sum of those above average we % the amount of students to find 
+the percentage above class average
+
+once we get our number of grades, average grade, and percent grades above average we display it
+to the user using print function
+"""
+
